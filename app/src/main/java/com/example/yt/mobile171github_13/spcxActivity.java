19a928102af89a56740e307743e2222cb3befab8
1711605024 <PJWpjw123>
2019-01-03 09:30:56 +0800

    子界面

---
+++ app/src/main/java/com/example/yt/mobile171github_13/spcxActivity.java
@@ -0,0 +1,13 @@
+package com.example.yt.mobile171github_13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class spcxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spcx);
+    }
+}
