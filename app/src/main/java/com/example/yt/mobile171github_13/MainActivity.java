6fd8774f9b2fd1ac1e2ae7592e6ef775757132b9
qq379454711 <379454711@qq.com>
2019-01-03 09:48:14 +0800

    主函数代码

--- app/src/main/java/com/example/yt/mobile171github_13/MainActivity.java
+++ app/src/main/java/com/example/yt/mobile171github_13/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.yt.mobile171github_13;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edt_main_name,edt_main_password;
+    Button btn_main_dl;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+                edt_main_name=(EditText) findViewById(R.id.edt_main_name);
+                edt_main_password=(EditText) findViewById(R.id.edt_main_password);
+                btn_main_dl=(Button) findViewById(R.id.btn_main_dl);
+                btn_main_dl.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String name=edt_main_name.getText().toString().trim();
+                        String password=edt_main_password.getText().toString().trim();
+
+
+                    }
+                });
+
     }
 }
